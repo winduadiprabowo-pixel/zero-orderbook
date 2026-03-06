e7b6424845d8f4377da56ec3066ef074c3aeca6d
winduadiprabowo-pixel <winduadiprabowo@gmail.com>
2026-03-07 02:14:41 +0700

    feat: splash screen Ø animation

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
+
+// Hide splash after first paint
+requestAnimationFrame(() => {
+  requestAnimationFrame(() => {
+    (window as unknown as Record<string, () => void>).__splashDone?.();
+  });
+});
